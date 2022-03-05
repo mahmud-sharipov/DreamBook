f99d358a994a7cbef6fbf6167eacb4a821a509ff
Mahmud Sharipov <sharipov.m.97.97@outlook.com>
2022-03-05 23:33:30 +0500

    deploy steps

--- Dashboard/src/app/services/end-point.ts
+++ Dashboard/src/app/services/end-point.ts
@@ -3,5 +3,5 @@ import { InjectionToken } from '@angular/core';
 export let API_BASE_URL = new InjectionToken<string>('APIBaseUrl');
 
 export function getBaseApiUrl(): string {
-    return 'https://localhost:5001/api/';
+    return 'http://localhost:5000/api/';
 }
